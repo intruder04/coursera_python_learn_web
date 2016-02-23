8c2c1d32e011dd6414c5ad485f5947379c97ae54
intruder04 <may.viktor@gmail.com>
2016-02-23 10:47:57 +0300

    changed host

--- week 3 sockets/get_page.py
+++ week 3 sockets/get_page.py
@@ -1,7 +1,7 @@
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('www.py4inf.com', 80))
+mysock.connect(('www.pythonlearn.com', 80))
 mysock.send('GET http://www.pythonlearn.com/code/intro-short.txt HTTP/1.0\n\n')
 
 while True:
